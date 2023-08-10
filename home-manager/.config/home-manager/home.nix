53ef77fe10f6d9e3daf69e6abaf5f67f198bf48a
Przemek <przempore@gmail.com>
2023-08-10 14:10:04 +0200

    [home-manager] add `nvim` config

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -82,6 +82,44 @@ in
 
       userEmail = "przempore@gmail.com";
       userName = "Przemek";
+
+      delta = {
+        enable = true;
+        options = {
+          chameleon = {
+            dark = true;
+            line-numbers = true;
+            side-by-side = true;
+            keep-plus-minus-markers = true;
+            syntax-theme = "Nord";
+            file-style = "#434C5E bold";
+            file-decoration-style = "#434C5E ul";
+            file-added-label = "[+]";
+            file-copied-label = "[==]";
+            file-modified-label = "[*]";
+            file-removed-label = "[-]";
+            file-renamed-label = "[->]";
+            hunk-header-style = "omit";
+            line-numbers-left-format = " {nm:>1} │";
+            line-numbers-left-style = "red";
+            line-numbers-right-format = " {np:>1} │";
+            line-numbers-right-style = "green";
+            line-numbers-minus-style = "red italic black";
+            line-numbers-plus-style = "green italic black";
+            line-numbers-zero-style = "#434C5E italic";
+            minus-style = "bold red";
+            minus-emph-style = "bold red";
+            plus-style = "bold green";
+            plus-emph-style = "bold green";
+            zero-style = "syntax";
+            blame-code-style = "syntax";
+            blame-format = "{author:<18} ({commit:>7}) {timestamp:^12} ";
+            blame-palette = "#2E3440 #3B4252 #434C5E #4C566A";
+          };
+          features = "chameleon";
+          side-by-side = true;
+        };
+      };
     };
 
     fish = {
@@ -164,6 +202,45 @@ in
         vimPlugins.nvim-notify
         vimPlugins.nvim-lint
         vimPlugins.vim-go
+      ];
+
+      extraPackages = with pkgs; [
+        # languages
+        jsonnet
+        nodejs
+        python311Full
+        rustc
+
+        # language servers
+        gopls
+        haskell-language-server
+        jsonnet-language-server
+        lua-language-server
+        nil
+        nodePackages."bash-language-server"
+        nodePackages."diagnostic-languageserver"
+        nodePackages."dockerfile-language-server-nodejs"
+        nodePackages."pyright"
+        nodePackages."typescript"
+        nodePackages."typescript-language-server"
+        nodePackages."vscode-langservers-extracted"
+        nodePackages."yaml-language-server"
+        rust-analyzer
+        terraform-ls
+
+        # formatters
+        gofumpt
+        golines
+        nixpkgs-fmt
+        python310Packages.black
+        rustfmt
+
+        # tools
+        cargo
+        gcc
+        ghc
+        lazydocker
+        yarn
       ];
     };
   };
