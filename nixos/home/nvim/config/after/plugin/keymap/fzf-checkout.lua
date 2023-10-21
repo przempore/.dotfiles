f484aba14ddeb50d7de3ca3539d87ea007ec0a6b
Przemek <przempore@gmail.com>
2023-10-21 17:14:57 +0200

    [nix] remove nix config from here

--- nixos/home/nvim/config/after/plugin/keymap/fzf-checkout.lua
+++
@@ -1,4 +0,0 @@
-local nnoremap = require "my_config.keymap".nnoremap
-
-nnoremap("<leader>gc", ":GBranches<cr>", { noremap = true, silent = true, desc = "[Git] Branches"});
-nnoremap("<leader>gb", ":GBrowse<cr>", { noremap = true, silent = true, desc = "[Git] Browse"});
