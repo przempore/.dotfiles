4a0dc959945587badc8102602ee1a0566b97948c
Przemek <przempore@gmail.com>
2023-11-21 15:03:16 +0100

    [nvim] treesitter install nix

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -2,7 +2,7 @@ require'nvim-treesitter.configs'.setup {
     ensure_installed = {"c", "cpp", "bash", "cmake", "diff", "dockerfile", "dot", "fish", "git_rebase",
                         "gitattributes", "gitcommit", "gitignore", "json", "markdown", "meson", "proto",
                         "go", "latex", "llvm", "make", "lua", "ninja", "perl", "ql", "qmljs", "scheme",
-                        "sql", "todotxt", "python", "rust", "sxhkdrc", "toml", "vim", "yaml"},
+                        "sql", "todotxt", "python", "rust", "sxhkdrc", "toml", "vim", "yaml", "nix"},
     sync_install = false,
 
     highlight = {
