18bcebf91a71475e315565bc8ff20777953bd30a
Przemek <przempore@gmail.com>
2023-12-13 10:13:21 +0100

    [nvim] move oil to `after`

--- nvim/.config/nvim/after/plugin/keymap/oil.lua
+++ nvim/.config/nvim/after/plugin/keymap/oil.lua
@@ -45,9 +45,9 @@ require("oil").setup({
   keymaps = {
     ["g?"] = "actions.show_help",
     ["<CR>"] = "actions.select",
-    ["<C-s>"] = "actions.select_vsplit",
-    ["<C-h>"] = "actions.select_split",
-    ["<C-t>"] = "actions.select_tab",
+    -- ["<C-s>"] = "actions.select_vsplit",
+    -- ["<C-h>"] = "actions.select_split",
+    -- ["<C-t>"] = "actions.select_tab",
     ["<C-p>"] = "actions.preview",
     ["<C-c>"] = "actions.close",
     ["<C-l>"] = "actions.refresh",
