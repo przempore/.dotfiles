4703e08c8240ead11a49ab71134c93dc510dacf6
Przemek <przempore@gmail.com>
2024-03-19 18:22:36 +0100

    [bin/display_setup] move all windows to desktop1 before changing xrandr

--- bin/.local/bin/display_setup.sh
+++ bin/.local/bin/display_setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 [default|projector]"
+if [ $# -ne 1 ] || ([ "$1" != "default" ] && [ "$1" != "projector" ]); then
+    echo "Usage: $0 {default|projector}"
     exit 1
 fi
 
 ARG=$1
 
+bspc query -N -n .window | while read -r window_id; do bspc node "$window_id" -d ^1; done
+autorandr --load $ARG
 if [ "$ARG" == "default" ]; then
-    autorandr --load $ARG
     bspc monitor DP-2 -d 1 2 3 4 5 6 7 8 9 10
 elif [ "$ARG" == "projector" ]; then
-    autorandr --load $ARG
     bspc monitor HDMI-0 -d 1 2 3 4 5
     bspc monitor DP-2 -d 6 7 8 9 10
 fi
